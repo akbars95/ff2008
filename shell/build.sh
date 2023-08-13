c9725b6503a9cb04a3e958a03be8f63d7dba47b7
mtsmda <mynzat.dmitrii@gmail.com>
2023-08-13 21:07:50 +0300

    without base url in build angular command

--- shell/build.sh
+++ shell/build.sh
@@ -5,5 +5,6 @@ cd ..
 echo '----------------remove docs folder'
 rm -rfv ./docs
 echo '------------------finish'
-ng build --configuration production --base-href "./ford_fusion_2008" --progress --output-path ./docs --verbose
+#ng build --configuration production --base-href "./ford_fusion_2008" --progress --output-path ./docs --verbose
+ng build --configuration production --progress --output-path ./docs --verbose
 cd shell
