1ff23a8f63cbcfd1d63ea16444a82e489ff42075
mtsmda <mynzat.dmitrii@gmail.com>
2023-11-05 17:42:00 +0200

    small change

--- shell/build.sh
+++ shell/build.sh
@@ -2,9 +2,11 @@
 
 cd ..
 #ng build --output-path for_publish
+
 echo '----------------remove docs folder'
 rm -rfv ./docs
 echo '------------------finish'
+
 ng build --configuration production --base-href "/ford_fusion_2008/" --progress --output-path ./docs --verbose
 #ng build --configuration production --progress --output-path ./docs --verbose
 cp ./docs/index.html ./docs/404.html
